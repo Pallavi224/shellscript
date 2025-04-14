f371a76b2d7c0958af4e9b3f4c4f84575d7fa949
Pallavi224 <kumaripallavikec@gmail.com>
2025-04-14 13:24:34 +0530

    exist command

---
+++ LOOP_learning/exitcode.sh
@@ -0,0 +1,13 @@
+pwd
+
+#exit commad 
+# if 0 - comes its succesefully execute.
+# if 1 - comes its not executed suceesfully.#code status
+
+echo $?
+
+ls -op
+
+ls -op -i -u
+
+echo $?
---
+++ LOOP_learning/installation1.sh
@@ -0,0 +1,8 @@
+ command="Python"
+
+ if command -v $command ; then
+	 echo "its exist $commant"
+ else
+	 echo "it dosnt exist"
+ fi
+
