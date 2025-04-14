4e8947c4093be36145ac7acab2f0ccc4da352280
Pallavi224 <kumaripallavikec@gmail.com>
2025-04-14 13:59:31 +0530

    elaborating exit command

--- exitcommand.sh
+++ exitcommand.sh
@@ -13,14 +13,12 @@ Current_date=$(date +%Y-%m-%d)
 #mkdir -p "$Backup_location"
 
 # Check if the directory to backup exists and is not empty
-if [ -d "$Directory_to_backup" ] && [ "$(ls -A "$Directory_to_backup")" ]; then
-	# Create the tar.gz backup
-	tar -czf "$Backup_location/backup_$Current_date.tar.gz" "$Directory_to_backup"
-else
-	echo "Error: Directory $Directory_to_backup does not exist or is empty."
-	exit 1
-fi
-
+status=$?
 # Print a success message
 echo "Directory $Directory_to_backup has been backed up to $Backup_location/backup_$Current_date.tar.gz"
 
+if [ $status -eq 0 ]; then
+echo "Backup successful"
+else
+echo "Backup failed"
+fi
---
+++ test.sh
@@ -0,0 +1,10 @@
+./exitcommand.sh 
+
+echo $?
+
+if [ $? -eq 0 ]; then
+	echo "0"
+else
+	echo "1"
+
+fi
